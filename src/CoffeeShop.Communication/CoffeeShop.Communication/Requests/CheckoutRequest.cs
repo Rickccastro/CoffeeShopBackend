325097c41061c9247de934e0d20ec6379eeda6fb
Rickccastro <ricardoparmezancastro.dev@gmail.com>
2025-06-14 00:52:43 -0300

    feat: adicionar camada Communication com DTOs para Checkout

---
+++ src/CoffeeShop.Communication/CoffeeShop.Communication/Requests/CheckoutRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Communication.Requests
+{
+    public class CheckoutItem
+    {
+        public int Quantity { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public string ImageUrl { get; set; }
+    }
+
+    public class CheckoutRequest
+    {
+        public List<CheckoutItem> Items { get; set; }
+    }
+}
