069c2e19e6f4327b7b7ec16e4c826a5203ae0523
Rickccastro <ricardoparmezancastro.dev@gmail.com>
2025-06-14 00:52:53 -0300

    refactor: refatorar CheckoutSessionController para utilizar padrão Clean Architecture

---
+++ src/CoffeeShop.Api/Controllers/CheckoutSessionController.cs
@@ -0,0 +1,30 @@
+﻿using CoffeeShop.Application.UseCase.Checkout.Create;
+using CoffeeShop.Communication.Requests;
+using CoffeeShop.Communication.Responses;
+using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
+
+
+namespace CoffeeShop.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class CheckoutSessionController : ControllerBase
+{
+    [Route("create-checkout-session")]
+    [HttpPost]
+    public ActionResult Create([FromServices] ICreateCheckoutUseCase useCase, [FromBody] CheckoutRequest request)
+    {
+        var resultCreateCheckoutUseCase = useCase.CreateCheckout(request);
+
+        return Ok(new CheckoutSessionResponse(resultCreateCheckoutUseCase.ToString()));
+    }
+
+    [HttpGet]
+    public ActionResult SessionStatus([FromQuery] string session_id)
+    {
+        var sessionService = new SessionService();
+        Session session = sessionService.Get(session_id);
+
+        return new JsonResult(new { status = session.Status, customer_email = session.CustomerDetails.Email });
+    }
+}
