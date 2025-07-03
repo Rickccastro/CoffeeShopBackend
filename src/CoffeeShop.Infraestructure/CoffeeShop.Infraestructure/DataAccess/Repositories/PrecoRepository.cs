f0b20f666b15791d0e44de762fb054bcb97187c9
Rickccastro <ricardoparmezancastro.dev@gmail.com>
2025-07-03 01:34:25 -0300

    feat: criando data access e injeção de dependência para infraestrutura

---
+++ src/CoffeeShop.Infraestructure/CoffeeShop.Infraestructure/DataAccess/Repositories/PrecoRepository.cs
@@ -0,0 +1,28 @@
+﻿using CoffeeShop.Domain.Entities;
+using CoffeeShop.Domain.Repositories;
+using CoffeeShop.Infraestructure.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.Infraestructure.DataAccess.Repositories
+{
+    public class PrecoRepository : IPrecoRepository
+    {
+        private readonly CoffeeShopDbContext _context;
+
+        public PrecoRepository(CoffeeShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PriPrice> ObterPrecoVigenteAsync(Guid produtoId, DateTime dataReferencia)
+        {
+            return await _context.PriPrices
+                .AsNoTracking()
+                .Where(p => p.PriIdProduto == produtoId &&
+                            p.PriDataInicio <= dataReferencia &&
+                            (p.PriDataFim == null || p.PriDataFim >= dataReferencia))
+                .OrderByDescending(p => p.PriDataInicio)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
---
+++ src/CoffeeShop.Infraestructure/CoffeeShop.Infraestructure/DependencyInjectionExtension.cs
@@ -0,0 +1,36 @@
+﻿using CoffeeShop.Domain.Repositories;
+using CoffeeShop.Infraestructure.DataAccess.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Infraestructure
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            //services.AddScoped<IPasswordEncripter, Security.Cryptography.BCrypt>();
+            //services.AddScoped<ILoggedUser, LoggedUser>();
+
+            //AddToken(services, configuration);
+            AddRepositories(services);
+
+            //if (configuration.IsTestEnvironment() == false)
+            //{
+            //    AddDbContext(services, configuration);
+            //}
+        }
+
+        private static void AddRepositories(IServiceCollection services)
+        {
+            //services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IPrecoRepository, PrecoRepository>();
+        }
+    }
+}
