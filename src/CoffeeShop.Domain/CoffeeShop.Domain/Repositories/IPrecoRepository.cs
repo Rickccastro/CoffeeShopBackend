5b8b812dfaa7b715d071d4b79f53705aebcaa388
Rickccastro <ricardoparmezancastro.dev@gmail.com>
2025-07-03 01:34:17 -0300

    feat: criando repositórios

---
+++ src/CoffeeShop.Domain/CoffeeShop.Domain/Repositories/IPrecoRepository.cs
@@ -0,0 +1,14 @@
+﻿using CoffeeShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Domain.Repositories
+{
+    public interface IPrecoRepository
+    {
+        Task<PriPrice> ObterPrecoVigenteAsync(Guid produtoId, DateTime dataReferencia);
+    }
+}
---
+++ src/CoffeeShop.Domain/CoffeeShop.Domain/Repositories/IProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿using CoffeeShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Domain.Repositories
+{
+    public interface IProdutoRepository
+    {
+        Task<ProProduto> ObterPorIdAsync(Guid produtoId);
+    }
+}
