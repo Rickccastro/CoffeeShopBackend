8950f471a4ba3e1e1456637f30c95c2d2cdea083
Rickccastro <ricardoparmezancastro.dev@gmail.com>
2025-07-14 20:51:51 -0300

    feat: adicionar controller de envio de e-mail

---
+++ src/CoffeeShop.Api/Controllers/EmailController.cs
@@ -0,0 +1,22 @@
+﻿using CoffeeShop.Application.UseCase.Email.EmailServiceNotification;
+using CoffeeShop.Communication.Requests.Email;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        [Route("create-email")]
+        [HttpPost]
+        public async Task<ActionResult> Create([FromServices] IEmailSenderNotification useCase, [FromBody] EmailRequest request)
+        {
+            await useCase.SendEmailNotification(request.Email);
+
+            return Ok("Email Enviado");
+        }
+
+    }
+}
