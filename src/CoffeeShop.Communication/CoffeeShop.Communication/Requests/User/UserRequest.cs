582e1912f396c0741d3ee0cb4167ffb4678f0213
Rickccastro <ricardoparmezancastro.dev@gmail.com>
2025-07-14 20:51:54 -0300

    feat: adicionar contratos de request de usuário

---
+++ src/CoffeeShop.Communication/CoffeeShop.Communication/Requests/User/UserRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace CoffeeShop.Communication.Requests.User
+{
+    public class UserRequest
+    {
+        public required string Cpf { get; set; }
+        public required string Nome { get; set; }
+        public required string Email { get; set; }
+        public required string Senha { get; set; }
+        public required string Endereco { get; set; }
+    }
+}
