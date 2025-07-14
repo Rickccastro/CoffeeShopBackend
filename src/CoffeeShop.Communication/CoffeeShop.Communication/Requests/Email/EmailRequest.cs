eb5e2c48a37328f38b90bf6fff11c06d7fd37627
Rickccastro <ricardoparmezancastro.dev@gmail.com>
2025-07-14 20:51:53 -0300

    feat: adicionar contratos de request para envio de e-mail

---
+++ src/CoffeeShop.Communication/CoffeeShop.Communication/Requests/Email/EmailRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Communication.Requests.Email
+{
+    public class EmailRequest
+    {
+        public required string Email { get; set; }
+    }
+}
