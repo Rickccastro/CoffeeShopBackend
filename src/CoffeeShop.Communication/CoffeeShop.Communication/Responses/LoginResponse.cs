cd7b7970540ce3c5eceeca735fc64936ca073d8d
Rickccastro <ricardoparmezancastro.dev@gmail.com>
2025-07-14 20:51:54 -0300

    feat: adicionar contrato de resposta para login

---
+++ src/CoffeeShop.Communication/CoffeeShop.Communication/Responses/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace CoffeeShop.Communication.Responses
+{
+    public class LoginResponse
+    {
+        public required Guid Id { get; set; }
+        public required string Nome { get; set; }
+        public required string Email { get; set; }
+    }
+}
