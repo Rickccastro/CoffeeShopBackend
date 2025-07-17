4eee552ab49bb0be388e6db417e58d964125c951
Rickccastro <ricardoparmezancastro.dev@gmail.com>
2025-07-17 12:10:01 -0300

    feat(communication): add request and response models for checkout session

---
+++ src/CoffeeShop.Communication/CoffeeShop.Communication/Requests/Checkout/SessionResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace CoffeeShop.Communication.Requests.Checkout
+{
+    public class SessionResponse
+    {
+        public string Status { get; set; }
+        public string CustomerEmail { get; set; }
+    }
+}
