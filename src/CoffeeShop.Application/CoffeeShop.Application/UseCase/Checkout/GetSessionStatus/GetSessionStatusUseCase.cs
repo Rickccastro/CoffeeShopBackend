b16ffe41d8e45c16814cbdc451eb2d68dee3fe91
Rickccastro <ricardoparmezancastro.dev@gmail.com>
2025-07-17 11:53:18 -0300

    feat: add use case para o Session Status da Stripe

---
+++ src/CoffeeShop.Application/CoffeeShop.Application/UseCase/Checkout/GetSessionStatus/GetSessionStatusUseCase.cs
@@ -0,0 +1,21 @@
+﻿using CoffeeShop.Communication.Requests.Checkout;
+using CoffeeShop.Domain.ExternalServices.Stripe;
+using CoffeeShop.Domain.ExternalServices.Stripe.Entities;
+
+namespace CoffeeShop.Application.UseCase.Checkout.GetSessionStatus
+{
+    public class GetSessionStatusUseCase : IGetSessionStatusUseCase
+    {
+        private readonly IGetSessionStatus _getSessionStatus;
+
+        public GetSessionStatusUseCase(
+             IGetSessionStatus getSession)
+        {
+            _getSessionStatus = getSession;
+        }
+        public SessionStatus GetStatus(SessionRequest session)
+        {
+            return _getSessionStatus.GetStatus(session.SessionId);
+        }
+    }
+}
---
+++ src/CoffeeShop.Application/CoffeeShop.Application/UseCase/Checkout/GetSessionStatus/IGetSessionStatusUseCase.cs
@@ -0,0 +1,10 @@
+﻿using CoffeeShop.Communication.Requests.Checkout;
+using CoffeeShop.Domain.ExternalServices.Stripe.Entities;
+
+namespace CoffeeShop.Application.UseCase.Checkout.GetSessionStatus
+{
+    public interface IGetSessionStatusUseCase
+    {
+        SessionStatus GetStatus(SessionRequest sessionRequest);
+    }
+}
