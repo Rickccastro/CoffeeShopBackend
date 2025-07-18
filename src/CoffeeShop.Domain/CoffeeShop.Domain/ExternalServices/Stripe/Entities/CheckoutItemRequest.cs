cbc703286a7af0d5a26f2fb50d441a62baf441d4
Rickccastro <ricardoparmezancastro.dev@gmail.com>
2025-07-17 23:31:01 -0300

    feat: criando uma classe para representar um item para o checkout

--- src/CoffeeShop.Domain/CoffeeShop.Domain/ExternalServices/Stripe/Entities/CheckoutItemRequest.cs
+++ src/CoffeeShop.Domain/CoffeeShop.Domain/ExternalServices/Stripe/Entities/CheckoutItemRequest.cs
@@ -5,10 +5,11 @@
         public string ProdutoId { get; set; }         
         public string ProdutoNome { get; set; }
         public string ProdutoDescricao { get; set; }
-        public string ProdutoImagem { get; set; }
+        public string ProdutoImagem { get; set; }    
 
         public string PrecoId { get; set; }
         public long PrecoUnitario { get; set; }        
         public long Quantidade { get; set; }
+        public long ValorTotalItem { get; set; }
     }
 }
