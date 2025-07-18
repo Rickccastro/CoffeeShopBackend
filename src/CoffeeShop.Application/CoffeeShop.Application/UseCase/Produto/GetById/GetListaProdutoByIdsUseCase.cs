c19a88da576c0164e6c00ee68dff6b76837fe5b0
Rickccastro <ricardoparmezancastro.dev@gmail.com>
2025-07-17 23:20:45 -0300

    feat: criei caso de uso com Produto get lista de produtos pelo Id

---
+++ src/CoffeeShop.Application/CoffeeShop.Application/UseCase/Produto/GetById/GetListaProdutoByIdsUseCase.cs
@@ -0,0 +1,33 @@
+﻿using CoffeeShop.Communication.Requests.Checkout;
+using CoffeeShop.Domain.Entities;
+using CoffeeShop.Domain.Repositories.Especificas;
+
+namespace CoffeeShop.Application.UseCase.Produto.GetById
+{
+    public class GetListaProdutoByIdsUseCase : IGetListaProdutoByIdsUseCase
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public GetListaProdutoByIdsUseCase(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task<List<ProProduto>> GetListaProdutosAsync(List<CheckoutListItemRequest> items)
+        {
+            var listaIdsProduto = items.Select(x => x.ProdutoId).Distinct().ToList();
+
+            var produtos = await _produtoRepository.ObterListaIdsProdutosAsync(listaIdsProduto);
+
+            // Validação: garantir que todos os produtos foram encontrados
+            if (produtos.Count != listaIdsProduto.Count)
+            {
+                var encontrados = produtos.Select(p => p.ProIdProduto);
+                var faltantes = listaIdsProduto.Except(encontrados);
+                throw new InvalidOperationException($"Produtos não encontrados: {string.Join(", ", faltantes)}");
+            }
+
+            return produtos;
+        }
+    }
+}
---
+++ src/CoffeeShop.Application/CoffeeShop.Application/UseCase/Produto/GetById/IGetListaProdutoByIdsUseCase.cs
@@ -0,0 +1,11 @@
+﻿using CoffeeShop.Communication.Requests.Checkout;
+using CoffeeShop.Domain.Entities;
+
+namespace CoffeeShop.Application.UseCase.Produto.GetById
+{
+    public interface IGetListaProdutoByIdsUseCase
+    {
+        Task<List<ProProduto>> GetListaProdutosAsync(List<CheckoutListItemRequest> items);
+    }
+}
+    
