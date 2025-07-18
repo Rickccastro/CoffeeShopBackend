7f852edea15f35ed7584a0e0fd721136a75ac69b
Rickccastro <ricardoparmezancastro.dev@gmail.com>
2025-07-17 23:27:24 -0300

    refactor: separando responsabilidades do caso de uso do checkout

--- src/CoffeeShop.Application/CoffeeShop.Application/UseCase/Checkout/Create/CreateCheckoutUseCase.cs
+++ src/CoffeeShop.Application/CoffeeShop.Application/UseCase/Checkout/Create/CreateCheckoutUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CoffeeShop.Application.UseCase.Pedido.Create;
+﻿using CoffeeShop.Application.UseCase.Checkout.CreateCheckoutLineItems;
+using CoffeeShop.Application.UseCase.Pedido.Create;
 using CoffeeShop.Application.UseCase.PedidoItem.CreateListaPedidoItem;
 using CoffeeShop.Communication.Requests.Checkout;
 using CoffeeShop.Domain.ExternalServices.Stripe;
@@ -11,27 +12,32 @@ namespace CoffeeShop.Application.UseCase.Checkout.Create
     {
         private readonly ICreateCheckoutSession  _createCheckoutSession;
         private readonly ICreateListaPedidoItemUseCase _createListaPedidoItemUseCase;
+        private readonly ICreateCheckoutLineItemsUseCase _createCheckoutLineItemsUseCase;
         private readonly ICreatePedidoUseCase _createPedidoUseCase;
 
         public CreateCheckoutUseCase(
              IPedidoRepository pedidoRepository,
              ICreateCheckoutSession createCheckoutSession,
              ICreateListaPedidoItemUseCase createListaPedidoItemUseCase,
+             ICreateCheckoutLineItemsUseCase createCheckoutLineItemsUseCase,
              ICreatePedidoUseCase createPedidoUseCase)
         {
             _createCheckoutSession = createCheckoutSession;
             _createListaPedidoItemUseCase = createListaPedidoItemUseCase;
             _createPedidoUseCase = createPedidoUseCase;
+            _createCheckoutLineItemsUseCase = createCheckoutLineItemsUseCase;
         }
 
         public async Task<CheckoutSessionResult> CreateCheckout(CheckoutRequest request)
         {
-            var dataAtual = DateTime.UtcNow;    
-            var itensProcessados = await _createListaPedidoItemUseCase.ProcessarItensAsync(request.Items,dataAtual);
+            var dataAtual = DateTime.UtcNow;  
+            
+            var listaLineItems = await _createCheckoutLineItemsUseCase.ProcessarItensAsync(request.Items, dataAtual);
+            var listaPedidoItem = await _createListaPedidoItemUseCase.CreateListaPedidoItem(listaLineItems, dataAtual);
 
-            var session = _createCheckoutSession.CreateSession(itensProcessados.LineItems);
+            var session = _createCheckoutSession.CreateSession(listaLineItems);
 
-            await _createPedidoUseCase.CreatePedido(Guid.Parse(request.UserId), itensProcessados.PedidoItens, itensProcessados.ValorTotal, session);
+            await _createPedidoUseCase.CreatePedido(Guid.Parse(request.UserId), listaPedidoItem, session);
 
             return session;
         }
