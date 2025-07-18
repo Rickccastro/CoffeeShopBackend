0a8b04abf13b9e1d6c96207088393dfc23a2525f
Rickccastro <ricardoparmezancastro.dev@gmail.com>
2025-07-17 23:19:52 -0300

    feat: caso de uso com o Preco

---
+++ src/CoffeeShop.Application/CoffeeShop.Application/UseCase/Preco/GetPrecoVigente/GetPrecoVigenteUseCase.cs
@@ -0,0 +1,16 @@
+﻿using CoffeeShop.Application.UseCase.Preco.GetPrecoVigente;
+using CoffeeShop.Domain.Entities;
+
+namespace CoffeeShop.Application.UseCase.Preco.GetPrecoVIgente
+{
+    public class GetPrecoVigenteUseCase : IGetPrecoVigenteUseCase
+    {
+        public PriPrice GetPrecoVigente(ProProduto produto, DateTime dataAtual)
+        {
+            return produto.PriPrices
+                .FirstOrDefault(p => p.PriDataInicio <= dataAtual &&
+                                     (p.PriDataFim == null || p.PriDataFim >= dataAtual))
+                ?? throw new InvalidOperationException($"Preço vigente não encontrado para o produto {produto.ProIdProduto}.");
+        }
+    }
+}
---
+++ src/CoffeeShop.Application/CoffeeShop.Application/UseCase/Preco/GetPrecoVigente/IGetPrecoVigenteUseCase.cs
@@ -0,0 +1,9 @@
+﻿using CoffeeShop.Domain.Entities;
+
+namespace CoffeeShop.Application.UseCase.Preco.GetPrecoVigente
+{
+    public interface IGetPrecoVigenteUseCase
+    {
+        PriPrice GetPrecoVigente(ProProduto produto, DateTime dataAtual);
+    }
+}
