8d5037c402d183471d346b16b5f599c1325d18f4
Rickccastro <ricardoparmezancastro.dev@gmail.com>
2025-07-17 23:31:35 -0300

    refactor: adicionando metodo para buscar os produtos por uma lista de ids

--- src/CoffeeShop.Domain/CoffeeShop.Domain/Repositories/Especificas/IProdutoRepository.cs
+++ src/CoffeeShop.Domain/CoffeeShop.Domain/Repositories/Especificas/IProdutoRepository.cs
@@ -1,11 +1,10 @@
 ﻿using CoffeeShop.Domain.Entities;
-using System.Linq.Expressions;
 
 
 namespace CoffeeShop.Domain.Repositories.Especificas
 {
     public interface IProdutoRepository : IRepositoryBase<ProProduto>
     {
-        Task<ProProduto> ObterPorIdStringAsync(string id);
+        Task<List<ProProduto>> ObterListaIdsProdutosAsync(List<string> listaIdsProduto);
     }
 }
