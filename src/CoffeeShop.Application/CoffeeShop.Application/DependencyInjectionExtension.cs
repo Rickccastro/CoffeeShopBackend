8277ee1009bcde3cb83947f477805dae2a7f50ad
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-07-20 14:58:03 -0300

    feat: adicao da injeção de dependencia dos casos de uso

--- src/CoffeeShop.Application/CoffeeShop.Application/DependencyInjectionExtension.cs
+++ src/CoffeeShop.Application/CoffeeShop.Application/DependencyInjectionExtension.cs
@@ -1,13 +1,18 @@
 ﻿using CoffeeShop.Application.AutoMapper;
 using CoffeeShop.Application.UseCase.Checkout.Create;
+using CoffeeShop.Application.UseCase.Checkout.CreateCheckoutLineItems;
 using CoffeeShop.Application.UseCase.Checkout.GetSessionStatus;
 using CoffeeShop.Application.UseCase.Customer.Create;
 using CoffeeShop.Application.UseCase.Customer.Login;
 using CoffeeShop.Application.UseCase.Email.EmailServiceNotification;
+using CoffeeShop.Application.UseCase.Pedido.Create;
 using CoffeeShop.Application.UseCase.Pedido.GetTotalValorPedido;
+using CoffeeShop.Application.UseCase.PedidoItem.CreateListaPedidoItem;
 using CoffeeShop.Application.UseCase.PedidoItem.CreatePedidoItem;
 using CoffeeShop.Application.UseCase.Preco.GetPrecoVigente;
 using CoffeeShop.Application.UseCase.Preco.GetPrecoVIgente;
+using CoffeeShop.Application.UseCase.Produto.GetById;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CoffeeShop.Application
@@ -35,6 +40,10 @@ namespace CoffeeShop.Application
             services.AddScoped<ICreatePedidoItemUseCase, CreatePedidoItemUseCase>();
             services.AddScoped<IGetPrecoVigenteUseCase, GetPrecoVigenteUseCase>();
             services.AddScoped<IGetTotalValorPedidoUseCase, GetTotalValorPedidoUseCase>();
+            services.AddScoped<ICreateListaPedidoItemUseCase, CreateListaPedidoItemUseCase>();
+            services.AddScoped<ICreateCheckoutLineItemsUseCase, CreateCheckoutLineItemsUseCase>();
+            services.AddScoped<IGetListaProdutoByIdsUseCase, GetListaProdutoByIdsUseCase>();
+            services.AddScoped<ICreatePedidoUseCase, CreatePedidoUseCase>();
         }
     }
 }
