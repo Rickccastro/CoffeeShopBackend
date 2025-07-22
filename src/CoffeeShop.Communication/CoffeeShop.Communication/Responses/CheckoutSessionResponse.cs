c172cc7aeda8d138fe98367ad2a90ed7f5020308
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-07-21 22:02:07 -0300

    feat: adicionando retorno do session id

--- src/CoffeeShop.Communication/CoffeeShop.Communication/Responses/CheckoutSessionResponse.cs
+++ src/CoffeeShop.Communication/CoffeeShop.Communication/Responses/CheckoutSessionResponse.cs
@@ -3,10 +3,13 @@
     public class CheckoutSessionResponse
     {
         public string ClientSecret { get; set; }
+        public string SessionId { get; set; }
 
-        public CheckoutSessionResponse(string clientSecret)
+        public CheckoutSessionResponse(string clientSecret, string sessionId)
         {
             ClientSecret = clientSecret;
+            SessionId = sessionId;
         }
+
     }
 }
