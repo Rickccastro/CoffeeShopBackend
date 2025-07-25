08ddae7764b86f689c5dc723d05349c039d9f1a3
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-07-24 22:14:03 -0300

    feat: novo controller p fluxo do Login

---
+++ src/CoffeeShop.Api/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using CoffeeShop.Application.UseCase.Login.Login;
+using CoffeeShop.Application.UseCase.Login.LoginValidado;
+using CoffeeShop.Communication.Requests.Customer;
+using CoffeeShop.Communication.Requests.Login;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Api.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class LoginController : ControllerBase
+{
+    [Route("login-user")]
+    [HttpPost]
+    public async Task<ActionResult> Login([FromServices] ILoginNotificationUseCase useCase, [FromBody] LoginRequest request)
+    {
+        await useCase.LoginNotification(request);
+
+        return Ok("Codigo Enviado");
+    }
+
+    [Route("validated-login-user")]
+    [HttpPost]
+    public async Task<ActionResult> ValidatedLogin([FromServices] ILoginValidadoUseCase useCase, [FromBody] LoginValidatedRequest request)
+    {
+        var result = await useCase.LoginValidado(request);
+
+        return Ok(result);
+    }
+}
