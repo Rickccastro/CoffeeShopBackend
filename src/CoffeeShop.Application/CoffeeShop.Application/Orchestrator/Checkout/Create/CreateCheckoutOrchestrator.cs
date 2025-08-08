834b39a83191fb97b35af334e97bb6ef4c1ba626
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-07 23:31:13 -0300

    refactor: criando a pasta de orquestrador para situações de chamar multiplos casos de uso

---
+++ src/CoffeeShop.Application/CoffeeShop.Application/Orchestrator/Checkout/Create/CreateCheckoutOrchestrator.cs
@@ -0,0 +1,44 @@
+﻿using CoffeeShop.Application.Services.ExternalServices.Contracts.Stripe;
+using CoffeeShop.Application.Services.ExternalServices.DTO.Stripe;
+using CoffeeShop.Application.Services.InternalServices;
+using CoffeeShop.Application.UseCase.Pedido.Create;
+using CoffeeShop.Application.UseCase.PedidoItem.CreateListaPedidoItem;
+using CoffeeShop.Communication.Requests.Checkout;
+using CoffeeShop.Domain.Repositories.Especificas;
+
+namespace CoffeeShop.Application.Orchestrator.Checkout.Create
+{
+    public class CreateCheckoutOrchestrator : ICreateCheckoutOrchestrator
+    {
+        private readonly ICreateCheckoutSession  _createCheckoutSession;
+        private readonly ICreateListaPedidoItemUseCase _createListaPedidoItemUseCase;
+        private readonly ICreatePedidoUseCase _createPedidoUseCase;
+        private readonly ILoggedUser _loggedUser;
+
+        public CreateCheckoutOrchestrator(
+             IOrderRepository pedidoRepository,
+             ICreateCheckoutSession createCheckoutSession,
+             ICreateListaPedidoItemUseCase createListaPedidoItemUseCase,
+             ICreatePedidoUseCase createPedidoUseCase,
+             ILoggedUser loggedUser)
+        {
+            _createCheckoutSession = createCheckoutSession;
+            _createListaPedidoItemUseCase = createListaPedidoItemUseCase;
+            _createPedidoUseCase = createPedidoUseCase;
+            _loggedUser = loggedUser;
+        }
+
+        public async Task<CheckoutSessionResult> CreateCheckout(CheckoutRequest request)
+        {
+            var loggedUser = await _loggedUser.Get();
+
+            var listaPedidoItem = await _createListaPedidoItemUseCase.CreateListaPedidoItem(request.Items);
+
+            var session = _createCheckoutSession.CreateSession(listaPedidoItem);
+
+            await _createPedidoUseCase.CreatePedido(loggedUser.UsrIdUser, listaPedidoItem, session);
+
+            return session;
+        }
+    }
+}
---
+++ src/CoffeeShop.Application/CoffeeShop.Application/Orchestrator/Checkout/Create/ICreateCheckoutOrchestrator.cs
@@ -0,0 +1,10 @@
+﻿using CoffeeShop.Application.Services.ExternalServices.DTO.Stripe;
+using CoffeeShop.Communication.Requests.Checkout;
+
+namespace CoffeeShop.Application.Orchestrator.Checkout.Create
+{
+    public interface ICreateCheckoutOrchestrator
+    {
+      Task<CheckoutSessionResult> CreateCheckout(CheckoutRequest request);
+    }
+}
