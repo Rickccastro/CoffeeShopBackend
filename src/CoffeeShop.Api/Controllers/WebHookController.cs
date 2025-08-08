f350a8419216cc7b19a9457bae86c6d0c5f7960d
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-07 23:30:02 -0300

    feat: adicionando controller para escutar webhooks da api de pagamento para atulizar o status dos pedidos etc

---
+++ src/CoffeeShop.Api/Controllers/WebHookController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stripe;
+
+
+namespace CoffeeShop.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class WebHookController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Webhook()
+    {
+        var json = await new StreamReader(Request.Body).ReadToEndAsync();
+        var stripeSignature = Request.Headers["Stripe-Signature"];
+
+        //await _stripeWebhookHandler.HandleEventAsync(json, stripeSignature);
+
+        return Ok();
+    }
+}
