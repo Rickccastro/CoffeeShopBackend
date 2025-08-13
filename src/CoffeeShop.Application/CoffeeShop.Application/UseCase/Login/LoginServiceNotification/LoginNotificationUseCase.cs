f62a6d2af1d615a904b2af2cf8c227fe63536c14
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-12 21:40:14 -0300

    feat: adicionando estilo ao envio do codigo do login

--- src/CoffeeShop.Application/CoffeeShop.Application/UseCase/Login/LoginServiceNotification/LoginNotificationUseCase.cs
+++ src/CoffeeShop.Application/CoffeeShop.Application/UseCase/Login/LoginServiceNotification/LoginNotificationUseCase.cs
@@ -8,9 +8,8 @@ namespace CoffeeShop.Application.UseCase.Login.Login
     public class LoginNotificationUseCase : ILoginNotificationUseCase
     {
         private readonly IUserRepository _userRepository;
-        private readonly ICacheService _cacheService; 
+        private readonly ICacheService _cacheService;
         private readonly ISesEmailService _sesEmailService;
-
         public LoginNotificationUseCase(
             IUserRepository userRepository,
             ICacheService cacheService,
@@ -33,7 +32,28 @@ namespace CoffeeShop.Application.UseCase.Login.Login
 
             var code = new Random().Next(100000, 999999).ToString();
             await _cacheService.StoreCodeAsync(user.SenServiceEmailNotifications.SenNmEmail, code, TimeSpan.FromMinutes(5));
-            await _sesEmailService.SendEmailAsync(user.SenServiceEmailNotifications.SenNmEmail, "Coffee-Shop: Codigo de Verificação", $"Seu código é: {code}");
+
+            var projectRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\.."));
+
+            var templatePath = Path.Combine(
+                projectRoot,
+                "CoffeeShop.Application",
+                "CoffeeShop.Application",
+                "EmailTemplate",
+                "LoginCodeTemplate.html"
+            );
+
+            // Lendo o HTML do template
+            var htmlContent = await File.ReadAllTextAsync(templatePath);
+
+            // Substituindo o placeholder pelo código real
+            htmlContent = htmlContent.Replace("{{codigo}}", code);
+
+            await _sesEmailService.SendEmailAsync(
+                user.SenServiceEmailNotifications.SenNmEmail,
+                "Coffee-Shop: Código de Verificação",
+                htmlContent
+            );
         }
     }
 }
